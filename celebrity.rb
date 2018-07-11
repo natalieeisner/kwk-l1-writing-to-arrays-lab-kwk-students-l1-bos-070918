598a36e1d566a6d74db16b18205fc8d0d2adaac4
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-11 19:57:01 +0000

    automatically backed up by learn

--- celebrity.rb
+++ celebrity.rb
@@ -1,10 +1,10 @@
-student_celebrities = { }
+student_celebrities = { } 
 
 student_celebrities["Megan"] = "Beyonce"
 student_celebrities["Grace"] = "Gustavo D."
 student_celebrities["Katie"] = "Miyazaki"
 student_celebrities["Celine"] = "Kylie Jenner"
 
+student_names = ["Natalie", "Cece", "Pelumi", "Sidney", "Hope", "Christina","Ella"]
 
-
-puts student-celebrities["Celine"]
+celebrities = ["Gigi Hadid", "Kendall Jenner", "Sam Smith", "Charlie Puth", "Romme Strigid", "Billie Eilish", "Rihanna"]
